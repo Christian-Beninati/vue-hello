9980bf7dd6a740bfa8a58301fe6fe5887ec10e37
Christian-Beninati <christianbeninati.info@gmail.com>
2023-06-16 13:15:54 +0100

    App-Vue

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 console.log('Vue OK', Vue);
 
+// Estrapolo il metodo creatApp
+const { createApp } = Vue;
+
+// Inizializzo l'app Vue
+const app = createApp({
+    data(){
+        return{
+            
+        }
+    }
+});
+
+// Monto nell'elemento (di partenza)
+app.mount('#root');
